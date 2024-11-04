4da7b10ea7892cbea7d6e198d56b4b0669560d2a
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2024-11-04 10:14:01 +0100

    added mock data at MCC level and completed anonymize script

--- endline/data/raw/anonymize.R
+++ endline/data/raw/anonymize.R
@@ -1,4 +1,3 @@
-#
 ### this scipt loads raw data from MCCs and dairy farmers, anonymizes and puts in public folder
 rm(list=ls())
 library(leaflet)
@@ -89,6 +88,29 @@ farmers <- farmers %>%
          everything(), 
          name_mcc, q51_name_prevx, q51_namex, q51_name_prev, q51_name)
 
-
-
 write.csv(farmers,paste(path,"data/public/farmers.csv", sep="/"), row.names=FALSE)
+
+
+###now for MCCs
+MCCs <- read.csv("latest_MCCs.csv")
+
+## drop location, names and contact details
+to_drop <- c("start","end","deviceid","simserial","phonenumber", "subscriberid","form_name","enumerator","sub","Village","mcc_name","mcc.gps","mcc._gps_latitude","mcc._gps_longitude","mcc._gps_altitude","mcc._gps_precision",              
+             "meta.instanceID",                              
+             "X_id",                                         
+             "X_uuid",                                       
+             "X_submission_time" ,                           
+             "X_date_modified",                              
+             "X_tags",                                      
+             "X_notes",                                      
+             "X_version",                                    
+             "X_duration",                                   
+             "X_submitted_by",                               
+             "X_total_media",                                
+             "X_media_count",                                
+             "X_media_all_received",                         
+             "X_xform_id")            
+MCCs <- MCCs[ , !(names(MCCs) %in% to_drop)]
+
+write.csv(MCCs,paste(path,"data/public/MCCs.csv", sep="/"), row.names=FALSE)
+
